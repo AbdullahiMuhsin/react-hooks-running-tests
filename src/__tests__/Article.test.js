d64f45d6ea84ca1818a37c1cba46f690d4c491fa
abdullahi abdi <abdullahimuxsin398@gmail.com>
2024-07-30 15:24:12 +0300

    Done

--- src/__tests__/Article.test.js
+++ src/__tests__/Article.test.js
@@ -1,9 +1,12 @@
+// import libraries needed for testing
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 import React from "react";
 
+// import the component you wrote
 import Article from "../components/Article";
 
+// test the component
 test("displays the text 'please pass this test'", () => {
   render(<Article />);
 
